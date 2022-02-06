f662372feecb91ed2e2d1c9f05ae7ef281d3a329
Denver Fernandes <sub367@gmail.com>
2022-02-06 23:07:43 +0000

    Add delay to retrieval and sort farms by APY

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fetchFarms = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(yieldYakStableFarmsUrl);
+    await page.waitForTimeout(3000);
 
     const farms = await page.evaluate(() => {
         const formatTVL = (value) => {
@@ -17,7 +18,8 @@ const fetchFarms = async () => {
             return value
                 .replaceAll('APY', '')
                 .replaceAll(' ', '')
-                .replaceAll('$', '');
+                .replaceAll('$', '')
+                .replaceAll('%', '');
         }        
         const stableCoinSymbols = ['USDC.e', 'USDT.e', 'DAI.e', 'MIM', 'AM3D', 'AC4D'];
         const farmElements = [...document.getElementsByClassName('card')];
@@ -25,13 +27,14 @@ const fetchFarms = async () => {
         return farmElements
         .map((farmElement) => {
             return {
-                provider: farmElement.getElementsByClassName('subtitle is-4')[0].textContent,
+                provider: farmElement.getElementsByClassName('subtitle is-4')[0].textContent.replaceAll(' ', ''),
                 coin: farmElement.getElementsByClassName('title is-2')[0].textContent,
                 tvl: formatTVL(farmElement.getElementsByClassName('box is-dark')[0].textContent),
                 farmAPY: formatAPY(farmElement.getElementsByClassName('box is-dark')[1].textContent)
             }
         })
-        .filter(farm => stableCoinSymbols.includes(farm.coin));
+        .filter(farm => stableCoinSymbols.includes(farm.coin) && farm.farmAPY !== 'tbd')
+        .sort((a, b) => new Number(b.farmAPY) - new Number(a.farmAPY));
     });
 
     console.log(farms);
