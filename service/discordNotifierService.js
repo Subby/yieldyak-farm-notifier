e0f04f45104b761b3d30da4a5bcc7e95fd218dd0
Denver Fernandes <denver.fernandes@sainsburys.co.uk>
2022-02-17 14:07:02 +0000

    Fix startup error and try mitigate discord channel fetching erro

--- service/discordNotifierService.js
+++ service/discordNotifierService.js
@@ -7,42 +7,45 @@ const discordBotChannelId = process.env.DISCORD_BOT_CHANNEL_ID;
 const sendFarmNotification = async (investedFarm, bestFarm) => {
 
     const difference = Number(bestFarm.farmAPY) - Number(investedFarm.farmAPY);
+    const farmEmbed = new MessageEmbed()
+    .setColor('#0EAB4D')
+    .setTitle(`Better farm found: ${bestFarm.provider} - ${bestFarm.coin}`)
+    .setDescription(`${difference}% APY difference`)
+    .setURL(`https://yieldyak.com/farms/detail/${bestFarm.contractAddress}`)
+    .setAuthor({ name: 'Yield Yak Farm Notifier Bot', iconURL: 'https://pbs.twimg.com/profile_images/1466883079310659585/nJmWTHN5_400x400.png'})
+    .setThumbnail('https://pbs.twimg.com/profile_images/1466883079310659585/nJmWTHN5_400x400.png')
+    .addFields(
+        { name: 'Best Farm', value: `${bestFarm.provider} - ${bestFarm.coin}` },
+        { name: 'Best Farm APY', value: `${bestFarm.farmAPY}%`, inline: true },
+        { name: 'Best Farm TVL', value: `${bestFarm.tvl}`, inline: true },
+    )
+    .addField('\u200B', '\u200B', true)
+    .addFields(
+        { name: 'Invested Farm', value: `${investedFarm.provider} - ${investedFarm.coin}` },
+        { name: 'Invested Farm APY', value: `${investedFarm.farmAPY}%`, inline: true },
+        { name: 'Invested Farm TVL', value: `${investedFarm.tvl}`, inline: true },
+    )
+    .setTimestamp();
+
     const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+    
 
     logger.info('Discord bot logging in...');
 
+    client.on('ready', async () => {
+        try {
+            logger.info('Sending farm notification embed');
+            const channel = client.channels.cache.get(discordBotChannelId);
+            await channel.send({ embeds: [farmEmbed] });
+        } catch (exception) {
+            logger.error('Error sending notification: %o', exception);
+        } finally {
+            logger.info('Discord bot logging out...');
+            client.destroy();
+        }
+    });
+
     await client.login(discordBotToken);
-
-    const farmEmbed = new MessageEmbed()
-        .setColor('#0EAB4D')
-        .setTitle(`Better farm found: ${bestFarm.provider} - ${bestFarm.coin}`)
-        .setDescription(`${difference}% APY difference`)
-        .setURL(`https://yieldyak.com/farms/detail/${bestFarm.contractAddress}`)
-        .setAuthor({ name: 'Yield Yak Farm Notifier Bot', iconURL: 'https://pbs.twimg.com/profile_images/1466883079310659585/nJmWTHN5_400x400.png'})
-        .setThumbnail('https://pbs.twimg.com/profile_images/1466883079310659585/nJmWTHN5_400x400.png')
-        .addFields(
-            { name: 'Best Farm', value: `${bestFarm.provider} - ${bestFarm.coin}` },
-            { name: 'Best Farm APY', value: `${bestFarm.farmAPY}%`, inline: true },
-            { name: 'Best Farm TVL', value: `${bestFarm.tvl}`, inline: true },
-        )
-        .addField('\u200B', '\u200B', true)
-        .addFields(
-            { name: 'Invested Farm', value: `${investedFarm.provider} - ${investedFarm.coin}` },
-            { name: 'Invested Farm APY', value: `${investedFarm.farmAPY}%`, inline: true },
-            { name: 'Invested Farm TVL', value: `${investedFarm.tvl}`, inline: true },
-        )
-        .setTimestamp();
-
-    try {
-        logger.info('Sending farm notification embed');
-        const channel = await client.channels.cache.get(discordBotChannelId);
-        await channel.send({ embeds: [farmEmbed] });
-    } catch (exception) {
-        log.error('Error sending notification: %o', exception);
-    } finally {
-        logger.info('Discord bot logging out...');
-        client.destroy();
-    }
 }
 
 export default sendFarmNotification;
